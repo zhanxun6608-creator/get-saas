bf090bdc7eced3c373fe38c1fd1702a82fbdbfd4
Zhang Xun <zhanxun6608@gmail.com>
2026-05-07 21:09:50 +0800

    Add root-level Privacy/ Terms pages to fix broken links from non-locale pages

---
+++ app/privacy/page.tsx
@@ -0,0 +1,5 @@
+import { PrivacyContent } from "@/components/privacy/privacy-content"
+
+export default function PrivacyPage() {
+  return <PrivacyContent />
+}
---
+++ app/terms/page.tsx
@@ -0,0 +1,5 @@
+import { TermsContent } from "@/components/terms/terms-content"
+
+export default function TermsPage() {
+  return <TermsContent />
+}
